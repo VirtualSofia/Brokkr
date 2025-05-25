9ada455c32675507d19ad36929c1637659809fc4
VirtualSofia <thevirtualsofia@gmail.com>
2025-05-24 21:18:09 -0700

    (fixed)Added Test Liquid, needs work

--- src/main/java/com/virtualsofia/brokkr/block/BlockRegistry.java
+++ src/main/java/com/virtualsofia/brokkr/block/BlockRegistry.java
@@ -6,7 +6,6 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.FlowingFluid;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import com.virtualsofia.brokkr.fluid.FluidRegistry;
@@ -14,10 +13,10 @@ import com.virtualsofia.brokkr.fluid.FluidRegistry;
 public class BlockRegistry {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Brokkr.MODID);
 
-
-    public static final DeferredBlock<LiquidBlock> TEST_FLUID_BLOCK
+    //The TestFluidBlock class should be able to work for any fluid, could be changed to BaseFluidBlock
+    public static final DeferredBlock<TestFluidBlock> TEST_FLUID_BLOCK
             = BLOCKS.register("test_fluid_block",
-            () -> new LiquidBlock((FlowingFluid) FluidRegistry.SOURCE_TEST_FLUID,
+            () -> new TestFluidBlock(FluidRegistry.SOURCE_TEST_FLUID,
                     BlockBehaviour.Properties.ofFullCopy(Blocks.LAVA).speedFactor(0.5f).noLootTable()));
     
 }
